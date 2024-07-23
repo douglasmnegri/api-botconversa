e663e2a1a7c09704f4c1d551e4df629f845d3b26
Douglas Melo <douglasmnegri@gmail.com>
2024-07-23 20:51:03 -0300

    add an endpoint to get quotes directly from the system

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // index.js
-require('dotenv').config()
-console.log(process.env)
-const express = require('express');
+require("dotenv").config();
+console.log(process.env);
+const express = require("express");
 const app = express();
 const port = 3001;
-const {router} = require('./configs.js');
+const { router } = require("./configs.js");
 
-const {calculateShirtPrice} = require('./run-price.js');
-const {calculateQuote} = require('./freight.js');
-const {getCEP} = require('./check-cep.js');
+const { calculateShirtPrice } = require("./run-price.js");
+const { calculateQuote } = require("./freight.js");
+const { getCEP } = require("./check-cep.js");
+const { printOrder } = require("./tiny.js");
 
 app.use(express.json());
-app.use('/api/config', router);
+app.use("/api/config", router);
 
-app.post('/api/calculate', async (req, res) => {
+app.post("/api/calculate", async (req, res) => {
   try {
     const postData = req.body;
     console.log(postData);
@@ -22,28 +23,28 @@ app.post('/api/calculate', async (req, res) => {
 
     // Send the processed data and price back in the response
     res.json({
-      message: 'BotConversa received this message successfully',
+      message: "BotConversa received this message successfully",
       processedData: postData,
       price: priceResult,
     });
-
-    
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error'});
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-app.post('/api/quote', calculateQuote)
+app.post("/api/quote", calculateQuote);
 
-app.post('/api/getcep', getCEP)
+app.post("/api/getcep", getCEP);
 
-app.get('/api/health', (req, res) => {
+app.post("/api/tiny", printOrder);
+
+app.get("/api/health", (req, res) => {
   // Handle the POST request
-  console.log('Message Received (GET)');
-  res.send('OK');
+  console.log("Message Received (GET)");
+  res.send("OK");
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
