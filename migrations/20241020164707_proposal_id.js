2cc5bb70852bec2f209e5ebb1b25c0c241ea9c6a
Douglas M. Negri <douglasmnegri@Douglass-Laptop.local>
2024-10-20 16:28:35 -0300

    add proposal id table

---
+++ migrations/20241020164707_proposal_id.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  // Create the table
+  await knex.schema.createTable("proposal_id", function (table) {
+    table.increments("id").primary();
+    table.timestamp("createdAt").defaultTo(knex.fn.now());
+  });
+
+  // Set the starting value of the auto-incrementing id to 1000
+  await knex.raw("ALTER SEQUENCE proposal_id_id_seq RESTART WITH 1000");
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("proposal_id");
+};
